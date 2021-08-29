bd7540a2ad9f3f742cc74c3f5bf2bb4b069911fd
Bloark <wildson.luiz@gmail.com>
2021-08-29 12:46:35 -0300

    Parte final do projeto

---
+++ src/components/ImageCard/index.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+import Skeleton from '../Skeleton'
+
+
+
+const Card = styled.div`
+    display:flex;
+    justify-content: center;
+    padding: 0px;
+    width: 90px;
+    height: 90px;
+    border-radius: 6px;
+    background-image: url(${(props) => props.photo});
+    background-size:cover;
+`;
+
+const Title = styled.span`
+    font-family: ${(props) => props.theme.fonts.regular};
+    color: #ffffff;
+    font-size: 14px;
+    text-align: center
+    margin: 12px;
+    margin-left: 5px;
+
+`;
+
+const ImageCard = ({ photo, title }) => {
+    const [imageLoaded, setImageLoaded] = useState(false);
+
+    useEffect(() => {
+        const imageLoader = new Image();
+        imageLoader.src = photo;
+        imageLoader.onload = () => setImageLoaded(true);
+    }, [photo]);
+
+    return (
+        <>
+            {imageLoaded ? (
+                <Card photo={photo}>
+                    <Title>{title}</Title>
+                </Card>
+            ) : <Skeleton width="90px" height="90px"/>}
+        </>
+    );
+};
+
+export default ImageCard;
